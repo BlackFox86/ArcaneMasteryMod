36f77a9d3f342ee3f01d92dc394ebc1a7604acb5
RedFox86 <redfoxgaming67@gmail.com>
2023-01-11 18:03:54 -0500

    Added fire and ice crystals

--- src/main/java/net/redfox/arcanemastery/ArcaneMastery.java
+++ src/main/java/net/redfox/arcanemastery/ArcaneMastery.java
@@ -1,14 +1,15 @@
 package net.redfox.arcanemastery;
 
 import net.fabricmc.api.ModInitializer;
+import net.redfox.arcanemastery.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ArcaneMastery implements ModInitializer {
 	public static final String MOD_ID = "arcanemastery";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
