73b74e0734780c8281707fb8a2f968676e82b8da
RedFox86 <redfoxgaming67@gmail.com>
2023-01-20 18:00:54 -0500

    Added a Frozen Staff

--- src/main/java/net/redfox/arcanemastery/item/ModItems.java
+++ src/main/java/net/redfox/arcanemastery/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.redfox.arcanemastery.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
@@ -8,11 +9,14 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.redfox.arcanemastery.ArcaneMastery;
+import net.redfox.arcanemastery.item.custom.FrozenStaffItem;
 
 public class ModItems {
-    public static final Item FIRE_CRYSTAL = registerItem("fire_crystal",new Item(new Item.Settings()),ModItemGroup.ARCANE_MASTERY);
-    public static final Item ICE_CRYSTAL = registerItem("ice_crystal",new Item(new Item.Settings()),ModItemGroup.ARCANE_MASTERY);
-    private static Item registerItem(String name, Item item, ItemGroup group) {
+    public static final Item FIRE_CRYSTAL = registerItem("fire_crystal",new Item(new Item.Settings()), ModItemGroup.ARCANE_MASTERY);
+    public static final Item ICE_CRYSTAL = registerItem("ice_crystal",new Item(new Item.Settings()), ModItemGroup.ARCANE_MASTERY);
+    public static final Item FROZEN_STAFF = registerItem("frozen_staff",new FrozenStaffItem(new Item.Settings().maxCount(1)), ModItemGroup.ARCANE_MASTERY);
+    private static Item registerItem(String name, Item item, ItemGroup tab) {
+        ItemGroupEvents.modifyEntriesEvent(tab).register(entries -> entries.add(item));
         return Registry.register(Registries.ITEM, new Identifier(ArcaneMastery.MOD_ID, name), item);
     }
     public static void registerModItems() {
---
+++ src/main/java/net/redfox/arcanemastery/item/custom/FrozenStaffItem.java
@@ -0,0 +1,27 @@
+package net.redfox.arcanemastery.item.custom;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+
+public class FrozenStaffItem extends Item {
+    public FrozenStaffItem(Settings settings) {
+        super(settings);
+    }
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        if(hand == Hand.MAIN_HAND) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 200, 9));
+            entity.damage(DamageSource.MAGIC,10);
+            user.getItemCooldownManager().set(this, 400);
+        }
+        return super.useOnEntity(stack, user, entity, hand);
+    }
+
+}
